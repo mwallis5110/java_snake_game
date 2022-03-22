9d971e125564b8e244858626eeaf81fab54692a4
Mason Wallis <mwallis5110@gmail.com>
2022-03-22 18:12:45 -0500

    Done

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -44,10 +44,10 @@ public class GamePanel extends JPanel implements ActionListener{
     public void draw(Graphics g) {
 
         if(running) {
-            for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
-            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-        }
+        //     for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+        //     g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+        //     g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        // }
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
@@ -58,6 +58,7 @@ public class GamePanel extends JPanel implements ActionListener{
             } 
             else {
                 g.setColor(new Color(45, 180, 0));
+                // g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255))); Sets snake random color
                 g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
@@ -137,8 +138,13 @@ public class GamePanel extends JPanel implements ActionListener{
         //Game Over text
         g.setColor(Color.red);
         g.setFont(new Font("Ink Free", Font.BOLD, 75));
-        FontMetrics metrics = getFontMetrics(g.getFont());
-        g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics1.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+        //Score
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
     }
 
     @Override
