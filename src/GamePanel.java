36f288cd92d18a695ba615d2d94141e11a542c1c
Mason Wallis <mwallis5110@gmail.com>
2022-03-22 14:16:23 -0500

    First commit

---
+++ src/GamePanel.java
@@ -0,0 +1,46 @@
+import javax.swing.JPanel;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+    GamePanel() {
+
+    }
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public voide gameOver(Graphics g) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }        
+    }
+
+}
